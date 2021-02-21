58d6e105c7c9d8dab8a0c0151601aef0e402900e
TheCoreWin <Sergduginn@bk.ru>
2021-02-21 11:07:52 +1000

    add package

---
+++ Assets/Plugins/SquareDinoHCCore/Editor/CheckerContrast.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SquareDino.ChackerContrast
+{
+    class CheckerContrast : EditorWindow
+    {
+        [MenuItem("Window/SquareDino/CheckerContrast")]
+        static void Init()
+        {
+            CheckerContrast window = (CheckerContrast) GetWindow(typeof(CheckerContrast));
+        }
+
+        private void OnGUI()
+        {
+
+        }
+    }
+}
