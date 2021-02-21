c5a42a688349fb8b2dbb8fb767228bee25ea0ca2
TheCoreWin <Sergduginn@bk.ru>
2021-02-21 11:41:16 +1000

    test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class NewBehaviourScript : EditorWindow
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PostProcessVolume
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
