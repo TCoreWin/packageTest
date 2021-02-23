25874cf4435ea3daed535cd67b8e15e1b26eec40
TheCoreWin <Sergduginn@bk.ru>
2021-02-23 18:34:35 +1000

    123

--- Assets/Plugins/SquareDinoHCCore/Editor/CheckerContrast.cs
+++ Assets/Plugins/SquareDinoHCCore/Editor/CheckerContrast.cs
@@ -1,91 +1,138 @@
-﻿using UnityEditor;
+﻿using UnityEngine.SceneManagement;
+using UnityEditor;
 using UnityEngine;
+
+#if UNITY_POST_PROCESSING_STACK_V2
 using UnityEngine.Rendering.PostProcessing;
+#endif
 
 namespace SquareDino.ChackerContrast
 {
     class CheckerContrast : EditorWindow
     {
-        private bool isHaveAsset;
-        private static GameObject go;
-        
         #if UNITY_POST_PROCESSING_STACK_V2
-            private PostProcessProfile profile;
-            private PostProcessLayer postProcessingLayer;
+            private static PostProcessProfile profile;
+            private static PostProcessLayer postProcessingLayer;
+            private static ColorGrading granding;
         #endif
-
-        private static bool haveAsset;
-    
+        
+        private static bool enable;
+        private const string enableKey = "enableKey";
+        
         [MenuItem("Window/SquareDino/CheckerContrast")]
         static void Init()
         {
-            CheckerContrast window = (CheckerContrast) GetWindow(typeof(CheckerContrast));
+            CheckerContrast window = (CheckerContrast) GetWindow(typeof(CheckerContrast), false, "CheckerContrast", true);
+            window.maxSize = new Vector2(300f, 65f);
+            window.minSize = window.maxSize;
         }
 
         private void OnGUI()
         {
 #if UNITY_POST_PROCESSING_STACK_V2
-            if (profile != null)
-                isHaveAsset = true;
-            else
-                isHaveAsset = false;
+            GUILayout.BeginHorizontal("box");
             
-            GUI.enabled = !isHaveAsset;
+            GUI.enabled = !enable;
             
-                if (GUILayout.Button("Create profile to PostProcessing"))
-                {
-                    if (profile == null)
-                    {                    
-                        profile = ScriptableObject.CreateInstance<PostProcessProfile>();
-                        AssetDatabase.CreateAsset(profile, "Assets/CheckerContrastProfile.asset");
-                        //EditorUtility.FocusProjectWindow();
-                        //Selection.activeObject = profile;
-                        
-                        var granding = new ColorGrading();
-                        granding.saturation.overrideState = true;
-                        granding.saturation.value = -100;
-                        
-                        var grandingLinq = profile.AddSettings(granding);
-                        grandingLinq.enabled.value = true;
-                        EditorUtility.SetDirty(profile);
-                        AssetDatabase.SaveAssets();
-                    }  
-                }
+            if (GUILayout.Button("Enable contrast", GUILayout.Height(50)))
+            {    
+                enable = true;    
+                EditorPrefs.SetInt(enableKey, 1);
+            }   
             
-            GUI.enabled = true;
-
-            GUI.enabled = isHaveAsset;
+            GUI.enabled = enable;
             
-            if (GUILayout.Button("Enable contrast"))
+            if (GUILayout.Button("Disable contrast", GUILayout.Height(50)))
             {
-                var camera = Camera.main;
-
-                if (go != null)
-                {
-                    go.transform.position = camera.transform.position;
-                }
+                enable = false;
+                EditorPrefs.SetInt(enableKey, 0);
+            } 
             
-                if (camera != null)
-                {    
-                    go = new GameObject("PostProcessing Volume");
-                    var boxCollier = go.AddComponent<BoxCollider>();
-                    boxCollier.size = new Vector3(1, 1, 1);
-                    
-                    go.transform.position = camera.transform.position;
-                    postProcessingLayer = camera.gameObject.AddComponent<PostProcessLayer>();
-                    postProcessingLayer.volumeLayer = ~0;
-                    var layer = go.AddComponent<PostProcessVolume>();
-                    layer.blendDistance = 1000;
-                    layer.profile = profile;
-                }
-            }
-
-            if (GUILayout.Button("Disable contrast"))
-            {
-                DestroyImmediate(go);
-                DestroyImmediate(postProcessingLayer);
-            }
+            GUILayout.EndHorizontal();
 #endif
+        }
+
+        private void OnEnable()
+        {
+            enable = EditorPrefs.GetInt(enableKey, 0) == 1;
+            
+            SceneManager.sceneLoaded -= SceneLoaded;
+            SceneManager.sceneLoaded += SceneLoaded;
+            EditorApplication.playModeStateChanged += TryDisable;
+        }
+
+        private void TryDisable(PlayModeStateChange mode)
+        {
+            if (mode == PlayModeStateChange.ExitingPlayMode)
+            {
+                Disable();
+            }
+        }
+
+        private void SceneLoaded(Scene arg0, LoadSceneMode loadSceneMode)
+        {
+            Disable();
+            Enable();
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= SceneLoaded;
+            EditorApplication.playModeStateChanged -= TryDisable;
+        }
+
+        private static void Enable()
+        {
+#if UNITY_POST_PROCESSING_STACK_V2            
+            if(!enable) return;
+            
+            profile = ScriptableObject.CreateInstance<PostProcessProfile>();
+            AssetDatabase.CreateAsset(profile, "Assets/CheckerContrastProfile.asset");
+            granding = ScriptableObject.CreateInstance<ColorGrading>();
+            AssetDatabase.CreateAsset(granding, "Assets/Granding.asset");
+            
+            granding.saturation.overrideState = true;
+            granding.saturation.value = -100;
+            profile.AddSettings(granding);
+            profile.settings[0].enabled.value = true;
+            
+            EditorApplication.update += EnableDelay;
+#endif
+        }
+
+        private static void EnableDelay()
+        {
+#if UNITY_POST_PROCESSING_STACK_V2            
+            var camera = Camera.main;
+
+            var checkerContrastGameObject = new GameObject("PostProcessing Volume");
+
+            var boxCollier = checkerContrastGameObject.AddComponent<BoxCollider>();
+            boxCollier.size = Vector3.one;
+
+            checkerContrastGameObject.transform.position = camera.transform.position;
+            checkerContrastGameObject.transform.SetParent(camera.transform);
+            
+            postProcessingLayer = camera.GetComponent<PostProcessLayer>(); 
+            
+            if(postProcessingLayer == null)
+                postProcessingLayer = camera.gameObject.AddComponent<PostProcessLayer>();
+
+            postProcessingLayer.volumeLayer = ~0;
+            postProcessingLayer.volumeTrigger = camera.transform;
+            
+            var layer = checkerContrastGameObject.AddComponent<PostProcessVolume>();
+            layer.blendDistance = 1000;
+            layer.profile = profile;
+            
+            EditorApplication.update -= EnableDelay;
+#endif
+        }
+
+        private static void Disable()
+        {
+            AssetDatabase.DeleteAsset("Assets/CheckerContrastProfile.asset");
+            AssetDatabase.DeleteAsset("Assets/Granding.asset");
         }
     }
 }
